2126c6f1a24a5badf625d9140d94a7d5b591d490
joy ebene <joyebene153@gmail.com>
2025-01-16 01:33:46 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <div className="h-12 text-red-500 p-4 flex items-center justify-between border-b-2 border-b-red-500 uppercase md:h-24 lg:px-20 xl:px-40">
       {/* LEFT LINKS */}
-      <div className="hidden md:flex gcp-4 flex-1">
+      <div className="hidden md:flex gap-4 flex-1">
         <Link href="/">Homepage</Link>
         <Link href="/menu">Menu</Link>
         <Link href="/contact">Contact</Link>
